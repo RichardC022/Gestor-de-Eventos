aa01da518771757b773bf94368ae4d226f5b6b4d
Richard22 <richard.cajas@unl.edu.ec>
2024-11-03 23:14:28 -0500

    creación de clases

---
+++ src/main/java/com/example/models/EventoPrivado.java
@@ -0,0 +1,19 @@
+package com.example.models;
+
+public class EventoPrivado {
+    private float precio;
+
+    public EventoPrivado(float precio){
+        this.precio = precio;
+    }
+
+    //gett and sett
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+ 
+}
